c072ecf49c230a767ec28f2061f05b70e04729af
Balazs Erdesz <balazs.erdesz@logmein.com>
2020-09-20 13:02:33 +0200

    Initial Commit

---
+++ TuraTalalo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TuraTalalo
+//
+//  Created by Balazs Erdesz on 2020. 09. 20..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
