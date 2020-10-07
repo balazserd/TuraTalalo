dc4548cb71c322db95085226981bf37cb2661421
Balazs Erdesz <ispain17@yahoo.com>
2020-10-07 19:20:33 +0200

    Improvements on drawing.

--- TuraTalalo/MapsforgeReader-Swift/Rendering/MapRenderer.swift
+++ TuraTalalo/MapsforgeReader-Swift/Rendering/MapRenderer.swift
@@ -18,7 +18,7 @@ final class MapRenderer {
         self.preferredLanguage = preferredLanguage
     }
 
-    func renderTile(keyed key: TileKey, in graphicsRenderer: UIGraphicsRenderer, tile: VectorTile, layerId: String, queryBuffer: Int) throws {
+    func renderTile(keyed key: TileKey, in graphicsRenderer: UIGraphicsImageRenderer, tile: VectorTile, layerId: String, queryBuffer: Int) throws -> UIImage {
         let targetRect = graphicsRenderer.format.bounds
 
         var boundingBox = CGRect()
@@ -31,27 +31,32 @@ final class MapRenderer {
         let targetExtents = CGRect(topLeft: targetExtentsTopLeftPoint, bottomRight: targetExtentsBottomRightPoint)
         
         let scale = targetExtents.width / targetRect.width
-        let extraWidth = (2 * CGFloat(queryBuffer) + targetRect.width) * scale - targetExtents.width
-        let extraHeight = (2 * CGFloat(queryBuffer) + targetRect.height) * scale - targetExtents.height
+        let extraWidth = ((2 * CGFloat(queryBuffer) + targetRect.width) * scale - targetExtents.width) / 2
+        let extraHeight = ((2 * CGFloat(queryBuffer) + targetRect.height) * scale - targetExtents.height) / 2
 
-        var queryExtents = targetExtents.insetBy(dx: -2 * extraWidth, dy: -2 * extraHeight)
+        var queryExtents = targetExtents.insetBy(dx: 2 * extraWidth, dy: 2 * extraHeight)
 
         let renderingTransformation = CGAffineTransform.identity
-            .scaledBy(x: 1 / scale, y: -1 / scale)
-            .translatedBy(x: -targetExtents.origin.x, y: -targetExtents.origin.y)
+            .scaledBy(x: 0.5 / scale, y: -0.5 / scale)
+            .translatedBy(x: -targetExtents.minX, y: -targetExtents.maxY)
 
-        do {
-            try graphicsRenderer.runDrawingActions({ uiGraphicsContext in
-                let cgContext = uiGraphicsContext.cgContext
-                cgContext.beginPath()
-                cgContext.addRect(targetExtents)
-                cgContext.closePath()
+        let image = graphicsRenderer.image { uiGraphicsContext in
+            let cgContext = uiGraphicsContext.cgContext
 
-            }, completionActions: nil)
-        } catch {
-            //TODO
+            cgContext.setFillColor(self.theme.mapBackground ?? UIColor.blue.cgColor)
+            cgContext.fill(graphicsRenderer.format.bounds)
+
+            cgContext.saveGState()
+//            cgContext.concatenate(renderingTransformation)
+
+//            cgContext.addRect(targetExtents)
+            cgContext.setStrokeColor(UIColor.black.cgColor)
+            cgContext.stroke(CGRect(x:100,y:100,width:100,height:100))
+
+            cgContext.restoreGState()
         }
 
+        return image
     }
 }
 
--- TuraTalalo/ViewController.swift
+++ TuraTalalo/ViewController.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mapSpriteView: SKView!
+    @IBOutlet weak var mapView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,28 +29,13 @@ class ViewController: UIViewController {
         let mapRenderer = MapRenderer(theme: renderTheme, preferredLanguage: "en")
         let uiRenderer = UIGraphicsImageRenderer(bounds: view.bounds)
 
-        try! mapRenderer.renderTile(keyed: key,
+        let mapTileImage = try! mapRenderer.renderTile(keyed: key,
                                     in: uiRenderer,
                                     tile: tile,
                                     layerId: renderTheme.defaultLayerName ?? "",
                                     queryBuffer: 256)
 
-//        let scene = SKScene(size: mapSpriteView.bounds.size)
-//        scene.anchorPoint = CGPoint(x: 0, y: 0.5)
-//
-//        var points = [
-//            CGPoint(x:   0, y:   0),
-//            CGPoint(x: 100, y: 100),
-//            CGPoint(x: 200, y: -50),
-//            CGPoint(x: 300, y:  30),
-//            CGPoint(x: 400, y:  20)
-//        ]
-//
-//        let pathNode = SKShapeNode(splinePoints: &points, count: points.count)
-//        scene.addChild(pathNode)
-//        mapSpriteView.presentScene(scene)
+        mapView.image = mapTileImage
     }
-
-
 }
 
